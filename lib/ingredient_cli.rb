28fc61f8711d28eecee6848661b68d92d8c643c5
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-09-02 17:40:14 -0500

    Initial Setup

---
+++ lib/ingredient_cli.rb
@@ -0,0 +1,6 @@
+require "ingredient_cli/version"
+
+module IngredientCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ingredient_cli/version.rb
@@ -0,0 +1,3 @@
+module IngredientCli
+  VERSION = "0.1.0"
+end
