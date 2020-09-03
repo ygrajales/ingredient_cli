40b64756e3ad4465ea088a3478f48d3958cee663
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-09-02 22:29:25 -0500

    Updated and Renamed Files

--- lib/environment.rb
+++ lib/environment.rb
@@ -1,4 +1,5 @@
-require "ingredient_cli/version"
+require_relative "ingredient_cli/version"
+require_relative "ingredient_cli/cli"
 
 module IngredientCli
   class Error < StandardError; end
---
+++ lib/ingredient_cli/cli.rb
@@ -0,0 +1,18 @@
+class Ingredient::Cli
+
+    def welcome
+        puts "Welcome, intrepid chefs!"
+        sleep 5
+        puts "Today, we are providing a list of dishes for you - with a twist."
+        sleep 5
+        puts "As fearless champions of the kitchen, you do not always need or want recipes."
+        sleep 5
+        puts "So once you choose your dish, we will provide a list of ingredients."
+        sleep 5
+        puts "Then, you decide the amounts you want - endlessly customizable dishes at the stroke of a keyboard!"
+        sleep 5
+        puts "Bon Appetit! - Buen Provecho! - Enjoy!"
+    end
+
+
+end
